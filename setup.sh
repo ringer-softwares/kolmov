709857405c1a6aa31523ffd8c4dc67654d113512
Micael Veríssimo de Araújo <micael.verissimo@lps.ufrj.br>
2020-02-11 19:15:06 +0000

    initializate kolmov

---
+++ setup.sh
@@ -0,0 +1,12 @@
+export KOLMOV_PATH=`pwd`
+
+rm -rf .__python__
+mkdir .__python__
+cd .__python__
+
+ln -s ../Gaugi/python Gaugi
+ln -s ../kolmov/python kolmov
+
+export PYTHONPATH=`pwd`:$PYTHONPATH
+export JUPYTER_PATH=$PYTHONPATH
+cd ../
