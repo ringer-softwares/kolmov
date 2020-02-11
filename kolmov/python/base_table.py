d6f52c1e3f56beaa0151ff692fe3bfd22532ed7b
Micael Veríssimo de Araújo <micael.verissimo@lps.ufrj.br>
2020-02-11 21:23:17 +0000

    begin the table construction

---
+++ kolmov/python/base_table.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+class table_info(object):
+
+    def __init__(self, list_of_tuned_file, ):
+        
+        self.tuned_file_list = list_of_tuned_file
+    
+    def fill_table(self):
+        pass
+
+    def dump_table(self, output_path, table_name):
+        pass
+    
