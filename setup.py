b95724d923343d3892593d1cf009aad5f4287cd7
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2020-06-01 14:13:37 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
 # Imports
 from setuptools import setup, find_packages
 
-# Loading requirements
-deps = []
-with open('requirements.txt', 'r') as f:
-  for line in f.readlines():
-    deps.append(line.rstrip("\n"))
-
 # Loading README file
 with open("README.md", "r") as fh:
   long_description = fh.read()
 
 setup(
   name = 'kolmov',
-  version = '1.0.0',
+  version = '1.0.1',
   license='GPL-3.0',
   description = 'A Framework to perfomance the cross validation for Ringer tunings',
   long_description = long_description,
@@ -23,7 +17,18 @@ setup(
   author_email = 'micael.verissimo@lps.ufrj.br, gabriel.milan@lps.ufrj.br, jodafons@lps.ufrj.br',
   url = 'https://github.com/micaelverissimo/kolmov',
   keywords = ['framework', 'validation', 'machine-learning', 'ai', 'plotting', 'data-visualization'],
-  install_requires=deps,
+  install_requires = [
+    'keras',
+    'numpy>=1.18.1',
+    'six>=1.12.0',
+    'scipy>=1.4.1',
+    'future',
+    'sklearn',
+    'scikit-learn>=0.22.1',
+    'Gaugi>=1.0.0',
+    'matplotlib>=3.1.3',
+    'seaborn>=0.10.0'
+  ],
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
