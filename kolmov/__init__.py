c8b454b448b82e12f523cc426ac1a1e72d00c70d
jodafons <jodafons@lps.ufrj.br>
2020-12-23 22:51:04 -0300

    update

--- kolmov/__init__.py
+++ kolmov/__init__.py
@@ -13,16 +13,13 @@ from . import utils
 __all__.extend( utils.__all__ )
 from .utils import *
 
-if has_root:
-    from . import crossval_table
-    __all__.extend( crossval_table.__all__ )
-    from .crossval_table import *
+from . import crossval_table
+__all__.extend( crossval_table.__all__ )
+from .crossval_table import *
 
-    from . import fit_table
-    __all__.extend( fit_table.__all__ )
-    from .fit_table import *
-else:
-    print('fit_table and crossval_table not available becouse ROOT is not installed at your system.')
+from . import fit_table
+__all__.extend( fit_table.__all__ )
+from .fit_table import *
 
 
 
